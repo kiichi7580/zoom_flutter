dd15c07811f9efa696e5d957088872e3065e2741
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-02-15 14:57:47 +0900

    カスタムボタン作成

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,22 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:zoom_flutter/utils/colors.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({Key? key, required this.text}) : super(key: key);
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
   final String text;
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ElevatedButton(
-        onPressed: () {},
-        child: Text(
-          text,
-          style: TextStyle(
-            fontSize: 17,
-            color: Colors.white,
-          ),
-        ),
+        onPressed: onPressed,
         style: ElevatedButton.styleFrom(
           primary: buttonColor,
           minimumSize: const Size(double.infinity, 50),
@@ -26,6 +24,13 @@ class CustomButton extends StatelessWidget {
             side: const BorderSide(
               color: buttonColor,
             ),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 17,
+            color: Colors.white,
           ),
         ),
       ),
