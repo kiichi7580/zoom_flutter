bb5cc785bd871a3c6f25d51763283991911d5aaf
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-02-15 14:57:29 +0900

    showSnacBarウィジェット作成

---
+++ lib/utils/utils.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+showSnacBar(BuildContext context, String text) {
+  return ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(text),
+    ),
+  );
+}
